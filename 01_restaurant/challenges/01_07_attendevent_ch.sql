51e176ca0a522cc560a754b697216589f3345d66
thotunu1 <50336967+thotunu1@users.noreply.github.com>
2024-06-11 14:15:45 +0000

    01_07 Solution

--- 01_restaurant/challenges/01_07_attendevent_ch.sql
+++ 01_restaurant/challenges/01_07_attendevent_ch.sql
@@ -2,3 +2,12 @@
 
 -- The customer 'atapley2j@kinetecoinc.com' will be in
 -- attendance, and will bring 3 friends.
+
+insert into anniversaryAttendees
+  (CustomerID, PartySize)
+values ((SELECT CustomerID 
+        from Customers 
+        where email = 'atapley2j@kinetecoinc.com'),
+        4)
+
+Select * from anniversaryAttendees
